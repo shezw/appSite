e2906be8a4b240f2cecec9f5a442c5d94eb2248e
shezw <hello@shezw.com>
2023-11-12 16:48:11 +0800

    add getui lib

---
+++ server/library/getui/autoload.php
@@ -0,0 +1,13 @@
+﻿<?php
+require_once(dirname(__FILE__) . '/' . 'IGt.Push.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/IGt.AppMessage.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/IGt.TagMessage.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/IGt.APNPayload.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/template/IGt.BaseTemplate.php');
+require_once(dirname(__FILE__) . '/' . 'IGt.Batch.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/utils/AppConditions.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/template/notify/IGt.Notify.php');
+require_once(dirname(__FILE__) . '/' . 'igetui/IGt.MultiMedia.php');
+require_once(dirname(__FILE__) . '/' . 'payload/VOIPPayload.php');
+require_once ('igetui/template/IGt.RevokeTemplate.php');
+require_once ('igetui/template/IGt.StartActivityTemplate.php');
